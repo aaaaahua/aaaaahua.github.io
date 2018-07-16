67c7f43226f32b8b9c2e3cd6bff46d8c634163a5
aaaaahua <503630147@QQ.com>
2018-07-16 13:57:53 +0800

    是

--- test/js/font.js
+++ test/js/font.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // 移动端rem自适应计算规范
 window.onload = function () {
     calcRem();
@@ -16,23 +16,5 @@ function calcRem(){
     };
     let scale = bodyWidth / 750;
     html.style.fontSize = scale * 100 + 'px';
-=======
-// 移动端rem自适应计算规范
-window.onload = function () {
-    calcRem();
-};
-window.onresize = function () {
-    calcRem();
-};
-function calcRem(){
-    let bodyWidth = document.getElementsByTagName('body')[0].clientWidth;
-    let html = document.getElementsByTagName('html')[0];
-    //自适应最大宽度限制，避免页面资源展示失真
-    if (bodyWidth >= 750){
-        html.style.fontSize = 100 + 'px';
-        return
-    };
-    let scale = bodyWidth / 750;
-    html.style.fontSize = scale * 100 + 'px';
->>>>>>> 346b89219a7f0c5317767ff64c4286deda063ee3
-};
+
+}
